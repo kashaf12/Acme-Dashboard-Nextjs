c50c3fced0ea84c7f0f4258cff32083f9cfe8a61
kashaf12 <kashafaahmed@gmail.com>
2025-04-09 19:39:46 +0530

    fix: added customer list

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,7 @@
 import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
 
 export default async function Page() {
   const customers = await fetchCustomers();
@@ -8,14 +9,14 @@ export default async function Page() {
     customers.map((row) => fetchFilteredCustomers(row.name))
   ).then((results) => results.filter((result) => result !== null).flat());
 
-  console.log({ filteredCustomers, customers });
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-
-      <CustomersTable customers={filteredCustomers} />
+      <Suspense fallback={"Loading..."}>
+        <CustomersTable customers={filteredCustomers} />
+      </Suspense>
     </div>
   );
 }
