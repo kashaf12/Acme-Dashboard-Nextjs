aaf99fe3c2922d39c30d76f76e4f3179f561eb4f
kashaf12 <kashafaahmed@gmail.com>
2025-04-09 19:31:57 +0530

    fix: added customer list

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+  const filteredCustomers = await Promise.all(
+    customers.map((row) => fetchFilteredCustomers(row.name))
+  ).then((results) => results.filter((result) => result !== null).flat());
+
+  console.log({ filteredCustomers, customers });
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+
+      <CustomersTable customers={filteredCustomers} />
+    </div>
+  );
 }
